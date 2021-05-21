3452590fc73bef0c899f24705d4d650b9426214e
mahesh <maheshmaniya1997@gmail.com>
2021-05-21 21:48:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './components/home';
+import Header from './components/header'
+import Footer from './components/footer'
+import About from './components/about'
+import Contact from './components/contact'
+import Gallery from './components/gallery'
+import Services from './components/services'
+import single from './components/single'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/gallery' component={Gallery} />
+          <Route path='/services' component={Services} />
+          <Route path='/single' component={single} />
+          <Redirect to='/' />
+        </Switch>
+        <Footer />
+    </Router>
+       
     </div>
-  );
+  )
 }
 
 export default App;
